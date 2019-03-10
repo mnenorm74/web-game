7b25ffee4604fc4296a1caabb12e93bc45a727ee
digi <digi@skbkontur.ru>
2019-03-10 19:49:42 +0500

    Easier Dependency Injection

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,12 +1,18 @@
-using System;
+﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
+using WebGame.Domain;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
+        // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
+        public UsersController(IUserRepository userRepository)
+        {
+        }
+
         [HttpGet("{userId}")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
